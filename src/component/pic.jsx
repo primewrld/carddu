f32321e946f5be82e85f58d789f7c0c36c198c9c
primewrld <primewrld399@gmail.com>
2024-05-31 11:15:12 +0100

    new commit

--- src/component/pic.jsx
+++ src/component/pic.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import images from "./images/cats-removebg-preview.png"
+
 import "../App.css"
 const CatPic = () => {
     return (
         <div className="cat">
-            <img src={images}alt="cat" />
+            <img src="/path/to/your/images/cats-removebg-preview.png" alt="cat" />
             <div className="dott"> 
-        <box-icon type="solid" color="grey" name="grey"></box-icon>
+        
         <box-icon type="solid" name="square" color="grey"></box-icon>
         <box-icon type="solid" name="square" color="grey"></box-icon>
         <box-icon type="solid" name="square" color="grey"></box-icon>
